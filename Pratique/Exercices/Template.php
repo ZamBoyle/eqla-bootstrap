86423660ed05ed5abd1f9bb4f5d71c654ae78e2b
ZamBoyle <johnny.piette@gmail.com>
2021-02-16 21:02:25 +0100

    Updates

---
+++ Pratique/Exercices/Template.php
@@ -0,0 +1,18 @@
+<?php include "Includes/mybootstrap.php"; ?>
+<?php AddTop("Page Template"); ?>
+
+<!-- Votre code HTML à mettre en-dessous -->
+<div class="container-fluid text-center" style="padding-top: 15px;">
+    <div class="jumbotron">
+        <h1>Hello from Bootstrap</h1>
+        <p class="lead">Bootstrap un framework très sympa à utiliser</p>
+    </div>
+</div>
+
+
+
+
+
+
+
+<?php AddBottom(); ?>
