2418a85c381777efe3706a3b8a96ffb8a5ac95c7
ZamBoyle <johnny.piette@gmail.com>
2021-02-20 21:30:14 +0100

    Catch des erreurs dans les scripts d'updates

--- update.sh
+++ update.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
+# Johnny Piette & Matthieu Darfeuille
+
 git reset --hard origin/master
+exit_status=$?
+
+[ $exit_status -ne 0 ] && { echo "Can not \"reset\" Git's worktree ($exit_status)" >&2; exit $exit_status; }
+
 git pull --rebase --autostash
+exit_status=$?
+
+[ $exit_status -ne 0 ] && { echo "Failed to execute \"pull\" command ($exit_status)" >&2; exit $exit_status; }
