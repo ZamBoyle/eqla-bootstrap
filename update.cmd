a8ce60bb2612c997add9d4abceb37037f0aba18b
ZamBoyle <johnny.piette@gmail.com>
2021-02-20 21:34:44 +0100

    Update update.cmd

--- update.cmd
+++ update.cmd
@@ -1,2 +1,8 @@
+REM Johnny Piette & Matthieu Darfeuille
+@echo off
+
 git reset --hard origin/master
+if %ERRORLEVEL% NEQ 0 echo Failed to "reset" Git's workingtree (%ERRORLEVEL%) & pause & exit /b %ERRORLEVEL%
+
 git pull --rebase --autostash
+if %ERRORLEVEL% NEQ 0 echo Failed to execute Git "pull" command (%ERRORLEVEL%) & pause & exit /b %ERRORLEVEL%
