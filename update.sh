ee2bfefcf2b4b2f1b436a0124e87dfff08e7385e
ZamBoyle <johnny.piette@gmail.com>
2021-02-24 16:02:07 +0100

    Updates

--- update.sh
+++ update.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 # Johnny Piette & Matthieu Darfeuille
 
-git reset --hard origin/master
+# Backup au cas où...
+dt=`date '+%d-%m_%Y_%H:%M'`
+git add .
+git commit -a -m "Backup."
+git branch my-backup_$dt
+
+git fetch origin
 exit_status=$?
 
-[ $exit_status -ne 0 ] && { echo "Can not \"reset\" Git's worktree ($exit_status)" >&2; exit $exit_status; }
+[ $exit_status -ne 0 ] && { echo "Can not \"fetch\" command ($exit_status)" >&2; exit $exit_status; }
 
-git pull --rebase --autostash
+git reset --hard origin/master
 exit_status=$?
 
 [ $exit_status -ne 0 ] && { echo "Failed to execute \"pull\" command ($exit_status)" >&2; exit $exit_status; }
