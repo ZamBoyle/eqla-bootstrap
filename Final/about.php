178f6c94de556e410a1400bf2dfbef060c906fe2
ZamBoyle <johnny.piette@gmail.com>
2021-05-11 22:01:37 +0200

    Updates

--- Final/about.php
+++ Final/about.php
@@ -4,13 +4,13 @@
 
 <div class="container bg-light mt-2 border rounded">
     <h1 class="text-center">Notre Histoire</h1>
-    <div class='d-inline-block my-1 p-2 bg-light border rounded text-justify'>
+    <div class='d-inline-block my-1 p-2 bg-light border rounded text-justify w-100'>
         <h2>Qui se cache derrière <span class='font-weight-bold font-italic'>Crazy Cupcakes</span> ?</h2>
         <p>C'est notre Directeur Auguste Cupcake, un dévoreur de gâteaux, qui créa <span class='font-weight-bold font-italic'>Crazy Cupcakes</span>.</p>
         <p>Depuis sa plus grande enfance, il n'arrête pas de les mélanger à toutes les sauces.</p>
         <p>Il rencontra par la suite sa douce et tendre Véronique qui lui apprit la délicatesse et une autre technique pour faire de petits cake...</p>
     </div>
-    <div class='d-inline-block my-1 p-2 bg-light border rounded text-justify'>
+    <div class='d-inline-block my-1 p-2 bg-light border rounded text-justify w-100'>
         <h2>D'où est venu ce nom <span class='font-weight-bold font-italic'>Crazy Cupcakes</span> ?</h2>
         <img class='img-fluid img-thumbnail product float-left mr-2' src='./Images/cupcakefist.jpg' aria-label='Image du premier CupCake avec un poing à manger dressé vers le haut !' title>
         <p>Auguste Cupcake créa le premier cupcake lorsqu'il voulu faire rentrer son gâteau dans sa tasse de café à l'aide de son poing: une idée un peu folle/crazy !</p>
